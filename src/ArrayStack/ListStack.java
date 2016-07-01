83a8b95803b6e06db8b708377911f3ce67344a2c
Jason <jboy81@gmail.com>
2016-06-30 17:39:15 -0700

    created the list stack class

---
+++ src/ArrayStack/ListStack.java
@@ -0,0 +1,47 @@
+package ArrayStack;
+
+public class ListStack<T> implements MyStackInterface<T> {
+
+	private T[] stackArray;
+	private int size;
+	private int count;
+	
+	public ListStack(){
+		this(5);
+	}
+	
+	public ListStack(int size){
+
+	}
+	
+	@Override
+	public void push(T newItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/ArrayStack/Node.java
@@ -0,0 +1,84 @@
+package ArrayStack;
+
+/**
+ * This class creates a node to be used in a linked list
+ * 
+ * 6/25/2016
+ * 
+ * @author jasonboyer
+ *
+ * @param <T>
+ */
+public class Node<T> {
+	private T data;
+	private Node nextNode;
+	private Node prevNode;
+	
+	/**
+	 *  Default constructor, set all to null
+	 */
+	public Node(){
+		data=null;
+		nextNode=null;
+		prevNode=null;
+	}
+	
+	/** 
+	 * Default constructor for just starting with data
+	 * @param data this is the data that will be added to the node
+	 */
+	public Node(T data) {
+		this.data=data;
+		nextNode=null;
+		prevNode=null;
+	}
+	
+	
+	/**
+	 * Constructor to initialize data and next node
+	 * 
+	 * @param data - data to be stored in the node
+	 * @param prevNode - pointer to the previous node
+	 */
+	private Node(T data, Node prevNode){
+		this.data=data;
+		this.nextNode=nextNode;
+		prevNode=null;
+	}
+	
+	/**
+	 * Constructor to create and initialize all values
+	 * 
+	 * @param data - data to be stored in the node
+	 * @param nextNode - pointer to the next node
+	 * @param prevNode - pointer to the previous node
+	 */
+	private Node(T data, Node prevNode, Node nextNode){
+		this.data=data;
+		this.nextNode = nextNode;
+		this.prevNode = prevNode;
+		
+	}
+	
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	public Node getNextNode() {
+		return nextNode;
+	}
+	public void setNextNode(Node nextNode) {
+		this.nextNode = nextNode;
+	}
+	public Node getPrevNode() {
+		return prevNode;
+	}
+	public void setPrevNode(Node prevNode) {
+		this.prevNode = prevNode;
+	}
+	
+	
+
+}
