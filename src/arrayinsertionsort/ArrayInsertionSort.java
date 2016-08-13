215b37149ab192a28e790bb61d05d269239bcda8
Jason <jboy81@gmail.com>
2016-08-12 17:51:06 -0700

    added base selectionsort

--- src/arrayinsertionsort/ArrayInsertionSort.java
+++ src/arrayinsertionsort/ArrayInsertionSort.java
@@ -1,5 +1,16 @@
 package arrayinsertionsort;
 
+import java.util.Arrays;
+
+/**
+ * @author jasonboyer
+ * 
+ * This class is an implementation of the selection sort. It is an exercise in using Java, generics, and refreshing my memory of
+ * Algorithms and data structures.
+ *
+ * @param <T>
+ */
+
 public class ArrayInsertionSort<T> {
 	public <T extends Comparable <? super T>> void insertionSort(T[] array){
 		insertionSort(array,false, false);
@@ -10,6 +21,22 @@ public class ArrayInsertionSort<T> {
 	}
 	
 	public <T extends Comparable <? super T>> void insertionSort(T[] array, boolean verbose, boolean reverse){
-
+		/* If the array has one or less elements, there is no point in sorting it */
+		if(array.length>1){
+			T temp = null;
+			int j;
+			for(int i=1;i<array.length;i++){
+				j=i-1;
+				temp = array[i];
+				System.out.println("array[i] " + array[i] + " temp " + temp + " array[j] " + array[j] + " compare " + temp.compareTo(array[j]) + " j " + j);
+				while(j>=0 && temp.compareTo(array[j])<0){
+					array[j+1] = array[j];
+					j--;
+				}
+				array[j+1]=temp;
+			}
+			
+		}
+		
 	}
 }
--- src/arrayinsertionsort/ArrayInsertionSortTest.java
+++ src/arrayinsertionsort/ArrayInsertionSortTest.java
@@ -6,9 +6,9 @@ public class ArrayInsertionSortTest {
 
 	public static void main(String[] args) {
 		
-		Integer[] intArray = {10,13,4,8,232,1,2,382,81,1};
-		Integer[] intArray2 = {10,13,4,8,232,1,2,382,81,1};
-		
+		//Integer[] intArray = {10,13,4,8,232,1,2,382,81,1};
+		//Integer[] intArray2 = {10,13,4,8,232,1,2,382,81,1};
+		Integer[] intArray = {3,1,2,5,3};
 		
 		ArrayInsertionSort<Integer> test = new ArrayInsertionSort<Integer>();
 		
@@ -16,12 +16,12 @@ public class ArrayInsertionSortTest {
 		test.insertionSort(intArray,true);
 		System.out.println("Sorted Array: " + Arrays.toString(intArray));
 		
-		
+		/*
 		System.out.println("Testing selection sort backward");
 		test.insertionSort(intArray,true,true);
 		System.out.println("Sorted Array: " + Arrays.toString(intArray));
 
-		
+		*/
 	}
 
 }
